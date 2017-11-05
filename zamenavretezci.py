5b37a6806c320dabe8e7aec82fd14c3df6dcbbc8
Petr-Pan <panek.pet@seznam.cz>
2017-11-05 11:16:39 +0100

    Add files via upload

---
+++ zamenavretezci.py
@@ -0,0 +1,11 @@
+def zamen(retezec, pozice, znak):
+   """Zameni jeden znak v retezci na zadane pozici."""
+   novyretezec = retezec[:pozice] + znak + retezec[pozice+1:]
+   return novyretezec
+
+
+
+
+
+
+
