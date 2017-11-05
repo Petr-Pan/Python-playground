e8294eda3fadaa3f37488799e2900ca3304184d8
Petr-Pan <panek.pet@seznam.cz>
2017-11-05 11:19:15 +0100

    Update and rename zamenavretezci.py to Lekce 5/HomeW/zamenavretezci.py

--- Lekce 5/HomeW/zamenavretezci.py
+++ Lekce 5/HomeW/zamenavretezci.py
@@ -2,10 +2,3 @@ def zamen(retezec, pozice, znak):
    """Zameni jeden znak v retezci na zadane pozici."""
    novyretezec = retezec[:pozice] + znak + retezec[pozice+1:]
    return novyretezec
-
-
-
-
-
-
-
